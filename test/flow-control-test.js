0067d203b9beb2be5e10052a26d10651e01462f7
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-17 21:28:19 +0000

    automatically backed up by learn

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -7,44 +7,6 @@ describe('flow-control', () => {
 
   before(done => {
     const src = path.resolve(__dirname, '..', 'flow-control.js')
-    function basicTeenager(age) {
-      if (age >= 13 && age <= 19) {
-        return "You are a teenager!"
-      }
-    }
-
-    function teenager(age) {
-      if (age >= 13 && age <= 19) {
-        return "You are a teenager!"
-      }
-
-      return "You are not a teenager"
-    }
-
-    function ageChecker(age) {
-      if (age >= 13 && age <= 19) {
-        return "You are a teenager!"
-      } else if (age < 13){
-        return "You are a kid"
-      }
-
-      return "You are a grownup"
-    }
-
-    function ternaryTeenager(age) {
-      age <= 12 ? return "You are a kid" : age <= 19 ? return "You are a teenager" : return "You are not a teenager"
-    }
-
-    function switchAge(age) {
-      switch (age) {
-        case age >= 13 && age <= 19:
-          return "You are a teenager!"
-          break;
-        default:
-          return "You have an age"
-      }
-    }
-
 
     jsdom.env('<div></div>', [src], (err, window) => {
       if (err) {
